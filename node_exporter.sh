1cd8aed059b4fbbe0daa64478d6f6bf1547c8009
razr07 <90617493+razr07@users.noreply.github.com>
2026-01-09 00:32:55 +0700

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -2,19 +2,20 @@
 # This script will install Node Exporter with systemd
 VERSION="1.10.2"
 sudo curl -LO "https://github.com/prometheus/node_exporter/releases/download/v$VERSION/node_exporter-$VERSION.linux-amd64.tar.gz"
-sudo tar xvfz node_exporter-$VERSION.linux-amd64.tar.gz -C /opt/
-sudo mv /opt/node_exporter-$VERSION.linux-amd64 /opt/node_exporter
-cat << EOF >node_exporter.service
-[Unit]
-Description=Node Exporter
+sudo tar xvfz node_exporter-$VERSION.linux-amd64.tar.gz 
+#-C /opt/
+sudo mv node_exporter-$VERSION.linux-amd64/node_exporter /opt/node_exporter/
+#cat << EOF >node_exporter.service
+#[Unit]
+#Description=Node Exporter
 
-[Service]
-User=root
-ExecStart=/opt/node_exporter/node_exporter
+#[Service]
+#User=root
+#ExecStart=/opt/node_exporter/node_exporter
 
-[Install]
-WantedBy=default.target
-EOF
+#[Install]
+#WantedBy=default.target
+#EOF
 #sudo mv node_exporter.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable node_exporter.service
